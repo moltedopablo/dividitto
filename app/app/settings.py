3c1f11e2e66a7493b8765dd513a62f8d675420e9
Pablo Moltedo <moltedopablo@gmail.com>
2023-08-28 21:25:45 -0300

    Added user login

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mathfilters',
     'main'
 ]
 
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login 
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
