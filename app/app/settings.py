10ff5643952662b273950c7f2c34d15088264e18
Pablo Moltedo <moltedopablo@gmail.com>
2023-08-31 12:37:34 -0300

    Add setting to log queries

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,6 +125,29 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Redirect to home URL after login 
+# Redirect to home URL after login
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login'
+
+# Uncomment to debug SQL Queries
+# LOGGING = {
+#     'version': 1,
+#     'filters': {
+#         'require_debug_true': {
+#             '()': 'django.utils.log.RequireDebugTrue',
+#         }
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'filters': ['require_debug_true'],
+#             'class': 'logging.StreamHandler',
+#         }
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#         }
+#     }
+# }
